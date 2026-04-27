46c9da2754b5204383052825d23f51bbfc5791be
Denys Boiko <denisbojko2005@gmail.com>
2026-04-27 13:07:44 +0200

    create ServiceResult

---
+++ czwiczenie7/Enums/ServiceResultStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace czwiczenie7.Enums;
+
+public enum ServiceResultStatus
+{
+    Success,
+    BadRequest,
+    NotFound,
+    Conflict    
+}
---
+++ czwiczenie7/Services/ServiceResult.cs
@@ -0,0 +1,35 @@
+﻿using czwiczenie7.Enums;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace czwiczenie7.Services;
+
+public class ServiceResult<T>
+{
+    public ServiceResultStatus Status { get; set; }
+    public T? Data { get; set; }
+    public string? ErrorMessage { get; set; }
+
+    public static ServiceResult<T> Ok(T data) => new()
+    {
+        Status = ServiceResultStatus.Success,
+        Data = data
+    };
+
+    public static ServiceResult<T> BadRequest(string message) => new()
+    {
+        Status = ServiceResultStatus.BadRequest,
+        ErrorMessage = message
+    };
+
+    public static ServiceResult<T> NotFound(string message) => new()
+    {
+        Status = ServiceResultStatus.NotFound,
+        ErrorMessage = message
+    };
+
+    public static ServiceResult<T> Conflict(string message) => new()
+    {
+        Status = ServiceResultStatus.Conflict,
+        ErrorMessage = message
+    };
+}
